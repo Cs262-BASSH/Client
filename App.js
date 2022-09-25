4dad160bc1796f9c3c851315b5c2fb1b8ac2780a
Ben <benedicto.krisno7@gmail.com>
2022-09-24 23:20:27 -0400

    tab features

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
+// import Login from './components/Login';
+// import SignIn from './components/SignIn';
+import Tabs from './components/Tabs';
+
 import { StatusBar } from 'expo-status-bar';
-import { Button, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 /* sell, search, buy */
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to KnightMarket</Text>
-      <StatusBar style="auto" />
 
-      {/* Login/Sign in Page */}
-      <View>
-        <Button title='Login In'></Button>
-        <Button title='Sign In'></Button>
-      </View>
-    </View>
+  return (
+    <NavigationContainer>
+      <StatusBar style="auto" />
+      <Tabs></Tabs>
+    </NavigationContainer>
   );
 }
 
