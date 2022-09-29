0842234f777ecf74eaefc2649c235f30d0370690
osuvwe1 <kerenogedegbe2003@gmail.com>
2022-09-29 19:59:25 -0400

    Create Splash Screen

--- components/Homepage.js
+++ components/Homepage.js
@@ -1,10 +1,11 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function Homepage() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>This is the homepage</Text>
+      
     </View>
   );
 }
