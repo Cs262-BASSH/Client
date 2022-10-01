3d3e76cccf573cf4b06d773d29a3ded5dc177e05
hk238 <hk238@calvin.edu>
2022-10-01 19:48:33 -0400

    seaching page commit

--- components/Search.js
+++ components/Search.js
@@ -1,28 +1,77 @@
 import React, {useState} from 'react';
-import { Text, View, StyleSheet , ScrollView} from 'react-native';
+import { Text, View, StyleSheet , ScrollView, TouchableOpacity} from 'react-native';
 import SearchBar from './SearchBar';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-export default function Search() {
+import Profile from './Profile';
+
+
+export default function Search( {navigation} ) {
     const [searchText, setSearchText] = useState();
   return (
-    <View style={styles.container}>
+  <View style={styles.container}>
+    <View>
       <SearchBar searchText={searchText} setSearchText={setSearchText}/>
-      <Text> Search page</Text>
+    </View>
 
-      <ScrollView> 
-        <View>
+    
+    <View>
+      <ScrollView style={styles.Iconsview}> 
+        
+        <View style = {styles.buttonraw}>
+          <TouchableOpacity style={styles.buttonView} onPress={() => navigation.navigate('Profile')}>
+            <Icon name="floor-lamp" size={50} color="#900" />
+            <Text style={styles.icontext}>lamp</Text>
+          </TouchableOpacity>
 
-          {/* 4 categories in a raw */}
+          <TouchableOpacity style={styles.buttonView}>
+            <Icon name="chair-rolling" size={50} color="#900" />
+            <Text style={styles.icontext}>chair</Text>
+          </TouchableOpacity>      
 
-          <View style={styles.buttonView}>
-          {/* https://reactnative.dev/docs/image */}
 
-          </View>
-           
-            
+          <TouchableOpacity style={styles.buttonView}>
+            <Icon name="table-furniture" size={50} color="#900" />
+            <Text style={styles.icontext}>Desk</Text>
+          </TouchableOpacity>  
+
         </View>
+
+        <View style = {styles.buttonraw}>
+
+          <TouchableOpacity style={styles.buttonView}>
+            <Icon name="tablet-android" size={50} color="#900" />
+            <Text style={styles.icontext}>electronics</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.buttonView}>
+            <Icon name="laptop" size={50} color="#900" />
+            <Text style={styles.icontext}>laptop and computer</Text>
+          </TouchableOpacity>      
+
+
+          <TouchableOpacity style={styles.buttonView}>
+            <Icon name="sofa" size={50} color="#900" />
+            <Text style={styles.icontext}>sofa</Text>
+          </TouchableOpacity>  
+
+        </View>
+
+
+        <View style = {styles.buttonraw}>
+
+          <TouchableOpacity style={styles.buttonView}>
+            <Icon name="dots-horizontal-circle" size={50} color="#900" />
+            <Text style={styles.icontext}>etc</Text>
+          </TouchableOpacity>
+
+
+        </View>
+
+
       </ScrollView>
     </View>
+  </View>
   );
 }
 
@@ -32,8 +81,28 @@ const styles = StyleSheet.create ({
         backgroundColor: '#FFF',
     },
 
+    buttonraw: {
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    },
+
     buttonView: {
-    width: '25%',
-    padding: 5,
+      width: '25%',
+      padding: 5,
+      alignItems: 'center',
+      justifyContent: 'space-around'
+    },
+
+
+    icontext: {
+        justifyContent: 'center'
+    },
+
+    Iconsview:{
+
     }
 })
+
+
+          {/* https://reactnative.dev/docs/image */}
+          {/* https://www.prudentdevs.club/btns-imgs-in-react-native */}
