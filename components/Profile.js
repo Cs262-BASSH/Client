a7db811b60769ed7a5db5d6f4e50d1a177b3d50b
wcsrchoi <wcsrchoi@gmail.com>
2022-10-02 21:08:12 -0400

    Updated profile

--- components/Profile.js
+++ components/Profile.js
@@ -1,10 +1,102 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import Settings from './Settings';
+import History from './profile_item/History'
+import Likes from './profile_item/Likes'
+import SavedItems from './profile_item/SavedItems'
+
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, Keyboard, ScrollView, Button, TouchableOpacity, FlatList } from 'react-native';
+
+function ProfileScreen({ navigation }) {
+
+
+  return (
+
+    <View style={styles.container}>
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}> -Hmm</Text>
+        <View style={styles.itemsList}>
+
+          {/* Touch Event */}
+          <TouchableOpacity>
+            <Text style={styles.items} onPress={() => navigation.navigate('Settings')}>Setting</Text>
+            <Text style={styles.items} onPress={() => navigation.navigate('History')}>Hisory</Text>
+            <Text style={styles.items} onPress={() => navigation.navigate('Likes')}>Likes</Text>
+            <Text style={styles.items} onPress={() => navigation.navigate('Saved Items')}>Saved Items</Text>
+          </TouchableOpacity>
+
+        </View>
+      </View>
+    </View>
+
+  );
+
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function Profile() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>This is the profile</Text>
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+      <Stack.Screen name="Settings" component={Settings} />
+      <Stack.Screen name="History" component={History} />
+      <Stack.Screen name="Likes" component={Likes} />
+      <Stack.Screen name="Saved Items" component={SavedItems} />
+    </Stack.Navigator>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  tasksWrapper: {
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  itemsList: {
+    marginTop: 130,
+
+  },
+  items: {
+    fontSize: 15,
+    padding: 15,
+    marginBottom: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    top: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 5,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addText: {},
+});
--- components/Search.js
+++ components/Search.js
@@ -1,108 +1,108 @@
-import React, {useState} from 'react';
-import { Text, View, StyleSheet , ScrollView, TouchableOpacity} from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
 import SearchBar from './SearchBar';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import Profile from './Profile';
 
 
-export default function Search( {navigation} ) {
-    const [searchText, setSearchText] = useState();
+export default function Search({ navigation }) {
+  const [searchText, setSearchText] = useState();
   return (
-  <View style={styles.container}>
-    <View>
-      <SearchBar searchText={searchText} setSearchText={setSearchText}/>
+    <View style={styles.container}>
+      <View>
+        <SearchBar searchText={searchText} setSearchText={setSearchText} />
+      </View>
+
+
+      <View>
+        <ScrollView style={styles.Iconsview}>
+
+          <View style={styles.buttonraw}>
+            <TouchableOpacity style={styles.buttonView} onPress={() => navigation.navigate('Profile')}>
+              <Icon name="floor-lamp" size={50} color="#900" />
+              <Text style={styles.icontext}>lamp</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.buttonView}>
+              <Icon name="chair-rolling" size={50} color="#900" />
+              <Text style={styles.icontext}>chair</Text>
+            </TouchableOpacity>
+
+
+            <TouchableOpacity style={styles.buttonView}>
+              <Icon name="table-furniture" size={50} color="#900" />
+              <Text style={styles.icontext}>Desk</Text>
+            </TouchableOpacity>
+
+          </View>
+
+          <View style={styles.buttonraw}>
+
+            <TouchableOpacity style={styles.buttonView}>
+              <Icon name="tablet-android" size={50} color="#900" />
+              <Text style={styles.icontext}>electronics</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.buttonView}>
+              <Icon name="laptop" size={50} color="#900" />
+              <Text style={styles.icontext}>laptop and computer</Text>
+            </TouchableOpacity>
+
+
+            <TouchableOpacity style={styles.buttonView}>
+              <Icon name="sofa" size={50} color="#900" />
+              <Text style={styles.icontext}>sofa</Text>
+            </TouchableOpacity>
+
+          </View>
+
+
+          <View style={styles.buttonraw}>
+
+            <TouchableOpacity style={styles.buttonView}>
+              <Icon name="dots-horizontal-circle" size={50} color="#900" />
+              <Text style={styles.icontext}>etc</Text>
+            </TouchableOpacity>
+
+
+          </View>
+
+
+        </ScrollView>
+      </View>
     </View>
-
-    
-    <View>
-      <ScrollView style={styles.Iconsview}> 
-        
-        <View style = {styles.buttonraw}>
-          <TouchableOpacity style={styles.buttonView} onPress={() => navigation.navigate('Profile')}>
-            <Icon name="floor-lamp" size={50} color="#900" />
-            <Text style={styles.icontext}>lamp</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.buttonView}>
-            <Icon name="chair-rolling" size={50} color="#900" />
-            <Text style={styles.icontext}>chair</Text>
-          </TouchableOpacity>      
-
-
-          <TouchableOpacity style={styles.buttonView}>
-            <Icon name="table-furniture" size={50} color="#900" />
-            <Text style={styles.icontext}>Desk</Text>
-          </TouchableOpacity>  
-
-        </View>
-
-        <View style = {styles.buttonraw}>
-
-          <TouchableOpacity style={styles.buttonView}>
-            <Icon name="tablet-android" size={50} color="#900" />
-            <Text style={styles.icontext}>electronics</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.buttonView}>
-            <Icon name="laptop" size={50} color="#900" />
-            <Text style={styles.icontext}>laptop and computer</Text>
-          </TouchableOpacity>      
-
-
-          <TouchableOpacity style={styles.buttonView}>
-            <Icon name="sofa" size={50} color="#900" />
-            <Text style={styles.icontext}>sofa</Text>
-          </TouchableOpacity>  
-
-        </View>
-
-
-        <View style = {styles.buttonraw}>
-
-          <TouchableOpacity style={styles.buttonView}>
-            <Icon name="dots-horizontal-circle" size={50} color="#900" />
-            <Text style={styles.icontext}>etc</Text>
-          </TouchableOpacity>
-
-
-        </View>
-
-
-      </ScrollView>
-    </View>
-  </View>
   );
 }
 
-const styles = StyleSheet.create ({
-    container : {
-        flex: 1,
-        backgroundColor: '#FFF',
-    },
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+  },
 
-    buttonraw: {
-        flexDirection: 'row',
-        justifyContent: 'space-around'
-    },
+  buttonraw: {
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
 
-    buttonView: {
-      width: '25%',
-      padding: 5,
-      alignItems: 'center',
-      justifyContent: 'space-around'
-    },
+  buttonView: {
+    width: '25%',
+    padding: 5,
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
 
 
-    icontext: {
-        justifyContent: 'center'
-    },
+  icontext: {
+    justifyContent: 'center'
+  },
 
-    Iconsview:{
+  Iconsview: {
 
-    }
+  }
 })
 
 
-          {/* https://reactnative.dev/docs/image */}
-          {/* https://www.prudentdevs.club/btns-imgs-in-react-native */}
+{/* https://reactnative.dev/docs/image */ }
+{/* https://www.prudentdevs.club/btns-imgs-in-react-native */ }
