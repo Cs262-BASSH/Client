d1c25e2c37a746d522b7ad9202aaec0abc3462e7
osuvwe1 <kerenogedegbe2003@gmail.com>
2022-10-06 12:45:22 -0400

    Make Icon and Splashpage

--- App.js
+++ App.js
@@ -1,25 +1,15 @@
 // import Login from './components/Login';
 // import SignIn from './components/SignIn';
-import Tabs from './components/Tabs';
 
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View} from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { NavigationContainer } from '@react-navigation/native';
-import SlashScreen from './components/SplashScreen';
-import SplashScreen from './components/SplashScreen';
+import {  StyleSheet } from "react-native";
+
+import Home from "./components/Home";
 
 /* sell, search, buy */
 export default function App() {
 
   return (
-    // <SafeAreaProvider>
-    //   <SplashScreen></SplashScreen>
-    // </SafeAreaProvider>
-    <NavigationContainer>
-      <StatusBar style="auto" />
-      <Tabs></Tabs>
-    </NavigationContainer>
+    <Home/>
   );
 }
 
