c0066507f02ddb885f93c81dfc43b4ded9f0dabb
hk238 <hk238@calvin.edu>
2022-10-19 16:20:31 -0400

    I made temperor hard coded data and applied to main page

--- screens/Homepage.js
+++ screens/Homepage.js
@@ -1,28 +1,61 @@
 import Sell from '../components/Sell'
-import { Text, View, StyleSheet, TouchableOpacity, Image, ScrollView, FlatList, Button, TouchableHighlight, Pressable } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, Image, ScrollView, FlatList, Button, TouchableHighlight, Pressable, SafeAreaView } from 'react-native';
+import items from '../data/categories'
+import { useState } from 'react';
 
 /*
   TODO: Change our hardcoded example in Homepage.js and Example.js to use data object instead of <Sell> and <smallSell>.
 */
 
 export default function Homepage() {
+  const [items, setitems] = useState([
+  {
+    name: "lava lamp", price: 8.8,
+    description: "coool lava lamp"
+},
+{
+    name: "small lava lamp", price: 8.7, 
+    description: 'small lava lamp'
+},
+{
+    name: "big lava lamp", price: 8.9, 
+    description: 'ig lava lamp'
+},
+{
+  name: "big lava lamp", price: 8.9, 
+  description: 'ig lava lamp'
+},
+{
+  name: "big lava lamp", price: 8.9, 
+  description: 'ig lava lamp'
+},
+{
+  name: "big lava lamp", price: 8.9, 
+  description: 'ig lava lamp'
+},
+]);
+
+
+
+
+
+
 
   return (
-    <ScrollView style={styles.container}>
-      {/* A hardcoded example */}
-      <Sell></Sell>
+<SafeAreaView style={{flex: 1}}>
 
       {/* A hardcoded example */}
-      <Sell></Sell>
-
-      {/* A hardcoded example */}
-      <Sell></Sell>
-
-      {/* A hardcoded example */}
-      <Sell></Sell>
-    </ScrollView>
+      <FlatList data={items} renderItem={({item}) => (
+        <Sell item = {item.name}  price = {item.price} description = 'this is a cool lava lamp'></Sell>
+      )} />
+      
+      </SafeAreaView>
   );
 }
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
