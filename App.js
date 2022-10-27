b67f31f5f69bed0afa7835c6c52115b9a28d3df1
osuvwe1 <kerenogedegbe2003@gmail.com>
2022-10-27 13:33:28 -0400

    Fix Login

--- App.js
+++ App.js
@@ -1,25 +1,47 @@
-// import Login from './components/Login';
-// import SignIn from './components/SignIn';
-
+import Login from './components/Login';
+import SignIn from './components/SignIn';
 import Home from "./screens/Home";
-import { View, StyleSheet} from 'react-native';
+import { View, StyleSheet,Image,Button,Dimensions, Alert, Text, TouchableOpacity} from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 /* sell, search, buy */
+const Stack = createNativeStackNavigator();
+
 export default function App() {
 
-  return (
-    <View style={styles.container}>
-      <Home/>
-    </View>
-  );
+  const {height, width} = Dimensions.get("window");
+    return (
+      <View style={styles.container}>
+          <View style={styles.header}>
+            <Image source={require('./assets/KNIGHTS.png')} height={height} width={width}/>
+          </View>
+          <View style={styles.footer}>
+            <Text style={styles.title}> Welcome Back!</Text>
+            <Button title='Login In'></Button>
+          </View>
+      </View>
+    )
 }
-
+    
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+  },
+  header : {
+    flex: 2,
     alignItems: 'center',
     justifyContent: 'center',
   },
-
+  footer : {
+    flex: 1,
+    backgroundColor: '#FFF',
+    paddingVertical: 10,
+    paddingHorizontal : 30,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  title : {
+   fontWeight: 'bold',
+  fontFamily: 'Optima',
+  }
 });
--- components/Login.js
+++ components/Login.js
@@ -1,21 +1,14 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { Button, View, Dimensions, Text, StyleSheet } from 'react-native'
+import { Button, Text, View } from 'react-native'
+import MaterialIcons from 'react-native-vector-icons';
+import Home from '../screens/Home';
 
 export default function Login() {
+
   return (
     <View>
-      <Button title='Login In'></Button>
+      <Home/>
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 20,
-    paddingTop: 20,
-  }
-});
