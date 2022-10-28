389e773465a8003f14a97f34ca0440b9ad18eae3
wcsrchoi <wcsrchoi@gmail.com>
2022-10-28 15:28:39 -0400

    commit

--- components/profile_item/History.js
+++ components/profile_item/History.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-export default function Settings() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>This is the History</Text>
-    </View>
-  )
+export default function History() {
+
+  if (Items !== null) {
+    return (
+
+
+
+      <View style={{ flex: 1, margintop: 50, marginLeft: 10 }}>
+
+        <SafeAreaView style={{ flex: 1 }}>
+
+          <FlatList data={Items} renderItem={({ item }) => (
+            <CategorySell item={item.name} price={item.price} description={item.description}></CategorySell>
+          )} />
+
+        </SafeAreaView>
+
+      </View>
+    )
+  }
+
+  else {
+    return (
+
+      <View style={{ flex: 1, margintop: 50, marginLeft: 10 }}>
+        You are not selling anytyhing.
+      </View>
+
+    )
+  }
 }
