f2b1695349fefc2bf817ecba3e8f95a6c5049430
wcsrchoi <wcsrchoi@gmail.com>
2022-10-28 15:54:18 -0400

    fix upload History

--- screens/Home.js
+++ screens/Home.js
@@ -1,31 +1,31 @@
 import Tabs from './Tabs';
 
 import React from 'react';
-import { View, StyleSheet, Image} from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 
 /* This is the home */
 
 
-export default function Home(){
+export default function Home() {
 
     return (
-    <View style={styles.container}>
-        <NavigationContainer>
-            <StatusBar barStyle='light-content' hidden={true}/>
-            <Tabs/>
-        </NavigationContainer>
-        <Image style={styles.logo_container} 
+        <View style={styles.container}>
+            <NavigationContainer>
+                <StatusBar barStyle='light-content' hidden={true} />
+                <Tabs />
+            </NavigationContainer>
+            {/* <Image style={styles.logo_container}
         source={require('../assets/logo.png')}
         height={50}
-        width={50}/>
-    </View>
+        width={50}/> */}
+        </View>
     );
 }
 
-const styles = StyleSheet.create ({
-    container : {
+const styles = StyleSheet.create({
+    container: {
         position: 'absolute',
         top: 0,
         bottom: 0,
